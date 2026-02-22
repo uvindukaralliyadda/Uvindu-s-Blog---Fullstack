431ae8867a94977a6bbf7392e1bd4a44cbdc60e1
uvindukaralliyadda <134487180+uvindukaralliyadda@users.noreply.github.com>
2026-02-22 23:04:36 +0530

    Mongo connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';  
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then (() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
 
 const app = express();
 
