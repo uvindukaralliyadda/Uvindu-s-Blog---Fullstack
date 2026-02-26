3237889b48544ac4b37f36e367b6519ce9b1a66e
uvindukaralliyadda <134487180+uvindukaralliyadda@users.noreply.github.com>
2026-02-26 16:38:06 +0530

    signin api created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 
 export const signup = async (req,res,next) => {
     const { username, email, password } = req.body;
@@ -30,7 +32,29 @@ export const signup = async (req,res,next) => {
 export const signin = async (req,res,next) => {  
     const { email, password } = req.body;
 
-    if(!email || !password || email === '' || password === '') {
-        return res.status(400).json({ message: 'All fields are required' });
-    }
+   if(!email || !password || email ==='' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
  } 
+
+  try {
+        const validUser = await User.findOne({ email });
+        if(!validUser) {
+           return next(errorHandler(404, 'User not found'));
+        }
+        const validPassword=bcrypt.compareSync(password, validUser.password);
+        if(!validPassword) {
+            return next(errorHandler(400, 'Invalid password'));
+        }
+
+        const token=jwt.sign( {id:validUser._id},process.env.JWT_SECRET, {expiresIn:'1d'});
+
+        //destructure password from validUser._doc and rest of the properties in rest   
+        const { password:past, ...rest } = validUser._doc;
+
+        res.status(200).cookie('access_token',token ,{
+            httpOnly: true,
+        }).json(rest);
+         }catch (error) {
+        next(error);
+  }
+ }
